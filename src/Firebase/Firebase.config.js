f860c01c0effb8835ea40f7a9323e14a4dd33261
Harisur-Sojol <harisursojol450@gmail.com>
2021-10-11 13:59:44 +0600

    code added

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBu0_Vm97KQZg9Q36C1zNEIIcOJF4h1fI4",
+    authDomain: "ema-john-with-auth-1d8f5.firebaseapp.com",
+    projectId: "ema-john-with-auth-1d8f5",
+    storageBucket: "ema-john-with-auth-1d8f5.appspot.com",
+    messagingSenderId: "708929468699",
+    appId: "1:708929468699:web:88ab3e37825b12b5ccfbbe"
+  };
+
+  export default firebaseConfig;
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+
+const initializeAuthentication = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default initializeAuthentication;
+
+/*
+steps for authentication
+----------------
+Step-1: Initial Setup
+1. firebase: create project
+2. create web app
+3. get configuration
+4. initialize firebase
+5. Enable auth method
+
+
+
+*/
