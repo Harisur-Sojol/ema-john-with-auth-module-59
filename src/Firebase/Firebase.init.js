b6080506fb4d9e3fe02109995d4ba99332a5567c
Harisur-Sojol <harisursojol450@gmail.com>
2021-10-11 22:46:59 +0600

    code updated

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -8,6 +8,7 @@ const initializeAuthentication = () => {
 
 export default initializeAuthentication;
 
+
 /*
 steps for authentication
 ----------------
@@ -18,6 +19,34 @@ Step-1: Initial Setup
 4. initialize firebase
 5. Enable auth method
 
+-------------------
+Step: 2
+1. Create Login Component
+2. Create Register Component
+3. create Route For Login And Register
 
+--------------------
+Step: 3
+1. set up sign in method
+2. set up sign out method
+3. user state
+4. special observer(useEffect)
+5. return necessary methods and states from useFirebase
 
+---------------------
+Step: 4
+1. Create a Auth context 
+2. Create context provider
+3. st context Provider Context value
+4. use Auth Provider 
+5. create useAuth Hook
+
+---------------------
+Step: 5
+1. Create Private Route
+2. Set Private Route
+
+---------------------
+Step: 6 :Redirect After Login
+1. after login redirect user to their desired destination
 */
